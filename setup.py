2a39ef079abeea1f92602936570e5a3c138a4f94
Anton Bobrov <a.bobrov@rambler-co.ru>
2012-12-21 11:52:04 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+    name='chll',
+    version='0.1',
+    description='cython HyperLogLog implementation compatible with java stream-lib',
+    author='Anton Bobrov',
+    author_email='bobrov@vl.ru',
+    url='https://github.com/baverman/chll',
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("chll", ["chll.pyx"], libraries=["m"])]
 )
